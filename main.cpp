80302cc37c977f9021309a0b59e6436680a3942c
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2023-07-02 12:11:19 -0300

    add input read

--- main.cpp
+++ main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include <vector>
 
 #define INF 0x3f3f3f3f
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-    // Your code here
-    return 0;
+
+int solve(vector<int> alloys){
+	return 0;
+}
+
+void printAlloys(vector<int> alloys){
+	for(auto a : alloys){
+		cout<<a<<' ';
+	}
+	cout<<endl;
+}
+
+int main(int argc, char const *argv[]) { 
+  unsigned int test_cases{0}, alloy_types{0}, order{0};
+	vector<int> alloys(alloy_types, 1);
+
+	cin>>test_cases;
+	while(cin>>alloy_types>>order){
+		for(unsigned int i=0; i<alloy_types; i++){
+			int buffer{0};
+			cin>>buffer;
+			alloys.push_back(buffer);
+		}
+
+
+		// printAlloys(alloys);
+		int minimum_alloys = solve(alloys);
+		cout<<minimum_alloys<<endl;
+		alloys.clear();
+	}
+
+
+
+  return 0;
 }
